ef0261510d93732d345419d826bc23d1afbfc9ca
guzen <guzenko.dmitriy@gmail.com>
2025-08-01 07:30:56 +0300

    map

---
+++ src/main/java/com/guzenko/test/collection/map_interface/HashTableExample.java
@@ -0,0 +1,20 @@
+package com.guzenko.test.collection.map_interface;
+
+import java.util.Hashtable;
+
+public class HashTableExample {
+    public static void main(String[] args) {
+        Hashtable<Double, Student> hashTable = new Hashtable<>();
+        Student student1 = new Student("Mike", "Mikelson", 2);
+        Student student2 = new Student("Kevin", "Magnusen", 5);
+        Student student3 = new Student("Maks", "Ferstappen", 1);
+        Student student4 = new Student("Lando", "Norris", 4);
+
+        hashTable.put(7.8, student1);
+        hashTable.put(9.3, student2);
+        hashTable.put(5.8, student3);
+        hashTable.put(6.0, student4);
+
+        System.out.println(hashTable);
+    }
+}
---
+++ src/main/java/com/guzenko/test/collection/map_interface/TreeMapExample.java
@@ -0,0 +1,48 @@
+package com.guzenko.test.collection.map_interface;
+
+import java.util.TreeMap;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<Double, Student> treeMap = new TreeMap<>();
+
+        Student student1 = new Student("David", "Davidson", 3);
+        Student student2 = new Student("John", "Johnson", 4);
+        Student student3 = new Student("Kate", "Stewart", 1);
+        Student student4 = new Student("Mike", "Mikelson", 2);
+        Student student5 = new Student("Kevin", "Magnusen", 5);
+        Student student6 = new Student("Maks", "Ferstappen", 1);
+        Student student7 = new Student("Lando", "Norris", 4);
+        Student student8 = new Student("Maks", "Ferstappen", 1);
+
+        treeMap.put(5.5, student4);
+        treeMap.put(8.5, student3);
+        treeMap.put(9.5, student7);
+        treeMap.put(7.5, student1);
+        treeMap.put(6.5, student5);
+        treeMap.put(7.9, student2);
+        treeMap.put(6.9, student6);
+
+//        treeMap.put(student4, 5.5);
+//        treeMap.put(student3, 8.5);
+//        treeMap.put(student7, 9.5);
+//        treeMap.put(student1, 7.5);
+//        treeMap.put(student5, 6.5);
+//        treeMap.put(student2, 7.9);
+//        treeMap.put(student6, 6.9);
+        System.out.println(treeMap);
+        System.out.println(treeMap.containsKey(student8));
+
+
+//        System.out.println(treeMap);
+//        System.out.println(treeMap.get(7.9));
+//        treeMap.remove(6.9);
+//        System.out.println(treeMap);
+//        System.out.println(treeMap.descendingMap());
+//        System.out.println(treeMap.tailMap(7.5));
+//        System.out.println(treeMap.headMap(7.5));
+//        System.out.println(treeMap.lastEntry());
+//        System.out.println(treeMap.firstEntry());
+
+    }
+}
